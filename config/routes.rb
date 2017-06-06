ea7dc7a06775de8c8f39ea2f3e412f9fb814014d
Adam Taufmann <ataufmann@gmail.com>
2017-06-06 09:21:49 -1000

    Made some useful comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'users#index'  #This makes the default web address go to the "/users" page
 end
